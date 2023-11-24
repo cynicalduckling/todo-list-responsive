4e93a13b7c89aaa1193ad5515cb66e763bb29464
rohithseby <48560429+rohithseby@users.noreply.github.com>
2023-11-25 01:12:35 +0530

    localstorage working. state management and sorting

--- script.js
+++ script.js
@@ -1,43 +1,148 @@
 let tasksContainer = document.querySelector("#tasks-container")
 let addTaskButton = document.querySelector("#add-task")
+let personal = document.querySelector("#personal")
+let work = document.querySelector("#work")
+let all = document.querySelector("#all")
+let today = document.querySelector("#today")
 
-let taskCounter = 0
-
-
-const deleteTask = e => {
-    document.getElementById(e.target.id).parentElement.parentElement.parentElement.style.display = "none"
-
-}
-
-let createTask = () => {
-
-    taskCounter = taskCounter + 1
-
-    taskHTML = `
-<div id="task-${taskCounter}" class="task-container">
+taskHTML = `
+<div class="task-container">
     <div class="task-details-row">
-        <input type="text" placeholder="Enter task" id="task-${taskCounter}-task" class="task-name-input task-content">
+        <input type="text" placeholder="Enter task" i class="task-name-input task-content">
     </div>
     <div class="task-content-row task-details-row">
-        <input type="date" id="task-${taskCounter}-date" class="task-details task-day task-details-bottom">
-        <select name="category-dropdown" id="task-${taskCounter}-category-dropdown"
+        <input type="date" class="task-details task-day task-details-bottom">
+        <select name="category-dropdown" class="task-category">
             class="task-details task-details-bottom task-category">
             <option value="personal">Personal</option>
             <option value="work">Work</option>
         </select>
-        <div class="delete-icons"><span class="material-symbols-outlined" id="delete-${taskCounter}-icon">delete</span>
+        <div class="delete-icons"><span class="material-symbols-outlined">delete</span>
         </div>
     </div>
 </div>`
 
-    currentInnerHTML = tasksContainer.innerHTML
-    tasksContainer.innerHTML = currentInnerHTML + taskHTML
-    deleteIcons = document.querySelectorAll(".delete-icons")
-    for (deleteIcon of deleteIcons) {
-        deleteIcon.addEventListener("click", deleteTask)
+
+const taskContainerTemp = new DOMParser().parseFromString(taskHTML, "text/html")
+const taskContainer = taskContainerTemp.documentElement.children[1].children[0]
+
+const createTaskElement = () => {
+    outerDiv = document.createElement("div")
+    outerDiv.classList.add("task-container")
+    firstInnerDiv = document.createElement("div")
+    firstInnerDiv.classList.add("task-details-row")
+    outerDiv.appendChild(firstInnerDiv)
+}
+
+const deleteTask = e => {
+    e.target.parentElement.parentElement.parentElement.remove()
+    renderTasks(updateTasks())
+}
+
+const setDefaultDate = () => {
+    for (let date of document.querySelectorAll("input[type='date']")) {
+        if (!date.value.length) {
+            date.defaultValue = new Date().toLocaleDateString('en-CA')
+        }
     }
 }
 
+const renderTasks = (tasks, cat = "all") => {
+    // if (cat === "personal" || cat === "work") {
+    //     finalTasks = tasks.filter((x) => x.taskCategory === cat)
+    // } else {
+    //     finalTasks = tasks
+    // }
+
+    // if (cat === "today") {
+    //     finalTasks = tasks.filter((x) => x.taskDate === new Date().toLocaleDateString('en-CA'))
+    // } else {
+    //     finalTasks = tasks
+    // }
+
+    let finalTasks = tasks.sort(function (a, b) {
+        let keyA = Date.parse(a.taskDate), keyB = Date.parse(b.taskDate)
+        if (keyA < keyB) return 1
+        if (keyA > keyB) return -1
+        return 0
+    })
+
+    tasksContainer.innerHTML = ""
+
+    for (let task of finalTasks) {
+        currentInnerHTML = tasksContainer.innerHTML
+        tasksContainer.innerHTML = taskHTML + currentInnerHTML
+    }
+
+    deleteIcons = document.querySelectorAll(".delete-icons")
+
+    for (deleteIcon of deleteIcons) {
+        deleteIcon.addEventListener("click", deleteTask)
+    }
+
+    let counter = 0
+
+    for (let task of document.querySelectorAll(".task-container")) {
+        task.querySelector("input[type='text']").value = finalTasks[counter].taskName
+        task.querySelector("input[type='date']").value = finalTasks[counter].taskDate
+        task.querySelector("select").value = finalTasks[counter].taskCategory
+        counter++
+    }
+
+    tasks = document.querySelectorAll(".task-container")
+
+    for (let task of tasks) {
+        task.addEventListener("change", () => {
+            setDefaultDate()
+            renderTasks(updateTasks())
+        })
+    }
+}
+
+const loadTasks = () => {
+    if ("todoTasks" in localStorage) {
+        renderTasks(JSON.parse(localStorage.todoTasks))
+    }
+}
+
+loadTasks()
+
+const updateTasks = () => {
+    tasks = document.querySelectorAll(".task-container")
+    const tasksJSON = []
+
+    for (let task of tasks) {
+        tasksJSON.push({
+            taskName: task.querySelector("input[type='text']").value,
+            taskDate: task.querySelector("input[type='date']").value,
+            taskCategory: task.querySelector("select").value,
+        })
+    }
+    localStorage.removeItem("todoTasks")
+    localStorage.setItem("todoTasks", JSON.stringify(tasksJSON))
+    globalTasks = tasksJSON
+    return tasksJSON
+}
+
+let createTask = () => {
+    setDefaultDate()
+    renderTasks(updateTasks())
+    tasksContainer.appendChild(taskContainer.cloneNode(true))
+    setDefaultDate()
+}
+
 addTaskButton.addEventListener("click", createTask)
+
+const filterTasks = (e) => {
+    category = e.target.id
+    console.log(category)
+    renderTasks(globalTasks, category)
+}
+
+personal.onclick = filterTasks
+work.onclick = filterTasks
+all.onclick = filterTasks
+today.onclick = filterTasks
+
 
 
